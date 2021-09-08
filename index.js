e168148e3eb33bb926bd3230d27c8e6efb128eee
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-08 15:20:09 +0100

    Add script to close the dropdown if user clicks outside of it

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function toggleDropDown() {
   document.querySelector('#myDropdown').classList.toggle('visible');
 }
 dropBtn.addEventListener('click', toggleDropDown);
+// Close the dropdown if user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.querySelectorAll('.dropdown-content');
+    for (let i = 0; i < dropdowns.length; i += 1) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('visible')) {
+        openDropdown.classList.remove('visible');
+      }
+    }
+  }
+};
